b390d036b1e432c777ac38806954f5e5e2991bc1
Vaibhav Verma <vaibhavv.co.17@nsit.net.in>
2025-03-15 01:43:12 +0530

    better-auth setup with drizzle coneection

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,6 @@ import { drizzle } from 'drizzle-orm/libsql';
 config({ path: '.env' });
 
 export const db = drizzle({ connection: {
-  url: process.env.TURSO_CONNECTION_URL!,
+  url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 }});
